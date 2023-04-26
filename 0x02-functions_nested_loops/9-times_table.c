4d193f84235ed534d9761bd3cc25df986ce5a2cf
Ivy153 <mariayvonne153@gmail.com>
2023-04-26 16:19:46 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,13 +3,9 @@
 
 
 /**
-
  * times_table - check description
-
  * Description: It prints 9 times table starting with 0
-
  * Return: Nothing.
-
  */
 
 void times_table(void)
@@ -26,53 +22,28 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 
 		{
-
 			n = i * j;
 
-
 			if ((n / 10) == 0)
-
 			{
-
 				if (j != 0)
-
 					_putchar(' ');
-
 				_putchar(n + '0');
-
-
 				if (j == 9)
-
 					continue;
-
 				_putchar(',');
-
 				_putchar(' ');
-
 			}
-
 			else
-
 			{
-
 				_putchar((n / 10) + '0');
-
 				_putchar((n % 10) + '0');
-
 				if (j == 9)
-
 					continue;
-
 				_putchar(',');
-
 				_putchar(' ');
-
 			}
-
 		}
-
 		_putchar('\n');
-
 	}
-
 }
